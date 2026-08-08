bba6e72d2a30811c1d9522d91b96e5c6721190c3
Emory Harris <lvvphole@gmail.com>
2026-08-08 13:23:44 -0400

    feat: vendor agent_standard package + inject behind AgentProvider

---
+++ src/specialized_harness/agent_standard/__init__.py
@@ -0,0 +1,17 @@
+from .runtime import (
+    AgentEngineeringStandard,
+    ContextBundle,
+    StandardError,
+    StandardConfigurationError,
+    UnknownProfileError,
+    UnknownLanguageOverlayError,
+)
+
+__all__ = [
+    "AgentEngineeringStandard",
+    "ContextBundle",
+    "StandardError",
+    "StandardConfigurationError",
+    "UnknownProfileError",
+    "UnknownLanguageOverlayError",
+]
